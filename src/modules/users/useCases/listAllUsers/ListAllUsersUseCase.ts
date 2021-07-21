a41d52971210dfe1e9e662138422eaf1d3e0420a
Leonel <leonelsc11@hotmail.com>
2021-07-21 09:36:23 -0300

    Final project

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+
+        if (!user || user.admin === false) {
+            throw new Error("You don't have permission to modify this user");
+        }
+        const allUsers = this.usersRepository.list();
+
+        return allUsers;
+    }
 }
 
 export { ListAllUsersUseCase };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
 import { app } from ".";
 
-const app = express():
-
 app.listen(3333, () => console.log("Server is running!"));
